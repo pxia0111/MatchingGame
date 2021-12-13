136b6f74f27f1783fc5bca021d22e7f7ce9fe185
unknown <ivan.ang@pundix.com>
2021-12-13 17:44:29 +0800

    netlify error

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import App from './components/App';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
- <App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
--- truffle-config.js
+++ truffle-config.js
@@ -22,8 +22,7 @@ module.exports = {
             confirmations: 2, // # of confs to wait between deployments. (default: 0)
             timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
             skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
-        }
-        
+        }        
         },
         
         contracts_directory: './src/contracts/',
